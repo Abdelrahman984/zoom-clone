58e6772ee60164ae0c804a74bd01ec6ca6d0401d
Abdelrahman <rafeeq220044@gmail.com>
2025-08-25 12:09:40 +0300

    Sidebar

---
+++ components/Sidebar.tsx
@@ -0,0 +1,48 @@
+"use client";
+import { sidebarLinks } from "@/constants";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+
+const Sidebar = () => {
+  const pathname = usePathname();
+  return (
+    <section
+      className="sticky top-0 left-0 flex flex-col w-fit h-screen
+    justify-between bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[264px]"
+    >
+      <div className="flex flex-col gap-6">
+        {sidebarLinks.map((link) => {
+          const isActive =
+            pathname === link.route || pathname.startsWith(link.route + "/");
+          return (
+            <Link
+              key={link.route}
+              href={link.route}
+              className={cn(
+                "flex gap-4 items-center p-4 rounded-lg justify-start",
+                {
+                  "bg-blue-1": isActive,
+                }
+              )}
+            >
+              <Image
+                src={link.imgUrl}
+                alt={link.label}
+                width={24}
+                height={24}
+              />
+              <p className="text-lg font-semibold max-lg:hidden">
+                {link.label}
+              </p>
+            </Link>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default Sidebar;
