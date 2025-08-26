6f89e7b876392e0fbaedf2e62e39c8d7e3f04735
Abdelrahman <rafeeq220044@gmail.com>
2025-08-26 17:57:56 +0300

    feat: add MeetingTypeList and MeetingModal components, enhance Home page layout, and update global styles

---
+++ components/HomeCard.tsx
@@ -0,0 +1,44 @@
+import { cn } from "@/lib/utils";
+import { Home } from "lucide-react";
+import Image from "next/image";
+import React from "react";
+
+interface HomeCardProps {
+  img: string;
+  title: string;
+  description: string;
+  handleClick: () => void;
+  className?: string;
+}
+const HomeCard = ({
+  img,
+  title,
+  description,
+  handleClick,
+  className,
+}: HomeCardProps) => {
+  return (
+    <div
+      className={`px-4 py-6 flex flex-col justify-between w-full
+        xl:max-w-[270px] min-h-[260px] rounded-[14px]
+        cursor-pointer ${className}`}
+      onClick={handleClick}
+    >
+      <div className="flex-center glassmorphism size-12 rounded-[10px]">
+        <Image
+          src={img}
+          alt="Meeting Type"
+          width={72}
+          height={72}
+          className="p-2"
+        />
+      </div>
+      <div className="flex flex-col gap-2">
+        <h1 className="text-2xl font-bold">{title}</h1>
+        <p className="text-lg font-normal">{description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default HomeCard;
