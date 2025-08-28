1dc82ea13978bf808446fdd99876beb09c62bc2e
Abdelrahman <rafeeq220044@gmail.com>
2025-08-28 20:36:41 +0300

    feat: enhance Personal Room and CallList components with new UI elements and functionality

--- app/(root)/(home)/personal-room/page.tsx
+++ app/(root)/(home)/personal-room/page.tsx
@@ -1,9 +1,79 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { useGetCallById } from "@/hooks/useGetCallById";
+import { useUser } from "@clerk/nextjs";
+import { useStreamVideoClient } from "@stream-io/video-react-sdk";
+import { useRouter } from "next/navigation";
 import React from "react";
+import { toast } from "sonner";
+
+const Table = ({
+  title,
+  description,
+}: {
+  title: string;
+  description: string;
+}) => {
+  return (
+    <div className="flex flex-col items-start gap-2 xl:flex-row">
+      <h1 className="text-base font-medium text-sky-1 lg:text-xl xl:min-w-32">
+        {title}:
+      </h1>
+      <h1 className="truncate text-sm font-bold max-sm:max-w-[320px] lg:text-xl">
+        {description}
+      </h1>
+    </div>
+  );
+};
 
 const PersonalRoom = () => {
+  const { user } = useUser();
+  const client = useStreamVideoClient();
+  const meetingId = user?.id || "guest-room";
+  const meetingLink = `${process.env.NEXT_PUBLIC_BASE_URL}/meeting/${meetingId}?personal=true`;
+  const { call } = useGetCallById(meetingId!);
+  const router = useRouter();
+
+  const startRoom = async () => {
+    if (!client || !user) return;
+    const newCall = client.call("default", meetingId!);
+    if (!call) {
+      await newCall.getOrCreate({
+        data: {
+          starts_at: new Date().toISOString(),
+        },
+      });
+      router.push(`/meeting/${meetingId}?personal=true`);
+    }
+  };
   return (
     <section className="flex flex-col gap-10 size-full text-white">
       <h1 className="text-3xl font-bold">Personal Room</h1>
+      <div className="flex w-full flex-col gap-8 xl:max-w-[900px]">
+        <Table
+          title="Topic"
+          description={`${user ? user.username : "Guest"}'s meeting room`}
+        />
+        <Table title="Meeting ID" description={meetingId!} />
+        <Table title="Invite ID" description={meetingLink!} />
+      </div>
+      <div className="flex gap-5">
+        <Button
+          className="bg-blue-1 hover:bg-blue-1/50 cursor-pointer"
+          onClick={startRoom}
+        >
+          Start Personal Room
+        </Button>
+        <Button
+          onClick={() => {
+            navigator.clipboard.writeText(meetingLink);
+            toast("Link Copied");
+          }}
+          className="bg-blue-1 hover:bg-blue-1/50 cursor-pointer"
+        >
+          Copy Invite Link
+        </Button>
+      </div>
     </section>
   );
 };
--- components/CallList.tsx
+++ components/CallList.tsx
@@ -7,14 +7,17 @@ import { useGetCalls } from "@/hooks/useGetCalls";
 import MeetingCard from "./MeetingCard";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
+import { Button } from "./ui/button";
 
 const CallList = ({ type }: { type: "ended" | "upcoming" | "recordings" }) => {
   const router = useRouter();
   const { endedCalls, upcomingCalls, callRecordings, isLoading } =
     useGetCalls();
   const [recordings, setRecordings] = useState<CallRecording[]>([]);
+  const [cleared, setCleared] = useState(false);
 
   const getCalls = () => {
+    if (cleared) return [];
     switch (type) {
       case "ended":
         return endedCalls;
@@ -60,51 +63,66 @@ const CallList = ({ type }: { type: "ended" | "upcoming" | "recordings" }) => {
 
   if (isLoading) return <Loader />;
 
-  const calls = getCalls();
+  const calls = getCalls() || [];
   const noCallsMessage = getNoCallsMessage();
 
   return (
-    <div className="grid grid-cols-1 gap-5 xl:grid-cols-2">
-      {calls && calls.length > 0 ? (
-        calls.map((meeting: Call | CallRecording) => (
-          <MeetingCard
-            key={(meeting as Call).id}
-            icon={
-              type === "ended"
-                ? "/icons/previous.svg"
-                : type === "upcoming"
-                ? "/icons/upcoming.svg"
-                : "/icons/recordings.svg"
-            }
-            title={
-              (meeting as Call).state?.custom?.description ||
-              (meeting as CallRecording).filename?.substring(0, 20) ||
-              "No Description"
-            }
-            date={
-              (meeting as Call).state?.startsAt?.toLocaleString() ||
-              (meeting as CallRecording).start_time?.toLocaleString()
-            }
-            isPreviousMeeting={type === "ended"}
-            link={
-              type === "recordings"
-                ? (meeting as CallRecording).url
-                : `${process.env.NEXT_PUBLIC_BASE_URL}/meeting/${
-                    (meeting as Call).id
-                  }`
-            }
-            buttonIcon1={type === "recordings" ? "/icons/play.svg" : undefined}
-            buttonText={type === "recordings" ? "Play" : "Start"}
-            handleClick={
-              type === "recordings"
-                ? () => router.push(`${(meeting as CallRecording).url}`)
-                : () => router.push(`/meeting/${(meeting as Call).id}`)
-            }
-          />
-        ))
-      ) : (
-        <h1 className="text-2xl font-bold text-white">{noCallsMessage}</h1>
+    <div>
+      {(type === "ended" || type === "recordings") && calls.length > 0 && (
+        <div className="mb-4 flex justify-end">
+          <Button
+            variant="destructive"
+            onClick={() => setCleared(true)}
+            className="px-4 py-2"
+          >
+            Clear History
+          </Button>
+        </div>
       )}
+      <div className="grid grid-cols-1 gap-5 xl:grid-cols-2">
+        {calls && calls.length > 0 ? (
+          calls.map((meeting: Call | CallRecording) => (
+            <MeetingCard
+              key={(meeting as Call).id}
+              icon={
+                type === "ended"
+                  ? "/icons/previous.svg"
+                  : type === "upcoming"
+                  ? "/icons/upcoming.svg"
+                  : "/icons/recordings.svg"
+              }
+              title={
+                (meeting as Call).state?.custom?.description ||
+                (meeting as CallRecording).filename?.substring(0, 20) ||
+                "No Description"
+              }
+              date={
+                (meeting as Call).state?.startsAt?.toLocaleString() ||
+                (meeting as CallRecording).start_time?.toLocaleString()
+              }
+              isPreviousMeeting={type === "ended"}
+              link={
+                type === "recordings"
+                  ? (meeting as CallRecording).url
+                  : `${process.env.NEXT_PUBLIC_BASE_URL}/meeting/${
+                      (meeting as Call).id
+                    }`
+              }
+              buttonIcon1={
+                type === "recordings" ? "/icons/play.svg" : undefined
+              }
+              buttonText={type === "recordings" ? "Play" : "Start"}
+              handleClick={
+                type === "recordings"
+                  ? () => router.push(`${(meeting as CallRecording).url}`)
+                  : () => router.push(`/meeting/${(meeting as Call).id}`)
+              }
+            />
+          ))
+        ) : (
+          <h1 className="text-2xl font-bold text-white">{noCallsMessage}</h1>
+        )}
+      </div>
     </div>
   );
 };
